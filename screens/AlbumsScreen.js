811c78a997145e9cad90f10a065ce173c5ccedc0
PointCodeZero <lucas_ftc@hotmail.com>
2019-03-01 13:58:21 -0500

    created album screen to display our albums

--- screens/AlbumsScreen.js
+++ screens/AlbumsScreen.js
@@ -1,15 +1,50 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
+import { CardList } from '../components/CardList';
 
 export default class AlbumsScreen extends React.Component {
   static navigationOptions = {
     title: 'Albums'
   };
 
+  constructor() {
+    super();
+    this.state = {
+      albums: [
+        {
+          title: 'Meteora',
+          image:
+            'https://cdn.shopify.com/s/files/1/0815/8995/products/linkin_park_meteora_grande.jpg?v=1428823373'
+        },
+        {
+          title: 'Meteora',
+          image:
+            'https://cdn.shopify.com/s/files/1/0815/8995/products/linkin_park_meteora_grande.jpg?v=1428823373'
+        },
+        {
+          title: 'Meteora',
+          image:
+            'https://cdn.shopify.com/s/files/1/0815/8995/products/linkin_park_meteora_grande.jpg?v=1428823373'
+        },
+        {
+          title: 'Meteora',
+          image:
+            'https://cdn.shopify.com/s/files/1/0815/8995/products/linkin_park_meteora_grande.jpg?v=1428823373'
+        }
+      ]
+    };
+  }
+
   render() {
+    const { albums } = this.state;
     return (
       <ScrollView style={styles.container}>
-        <Text>All Almbums</Text>
+        <CardList
+          data={albums}
+          imageKey={'image'}
+          titleKey={'title'}
+          buttonText="Details"
+        />
       </ScrollView>
     );
   }
