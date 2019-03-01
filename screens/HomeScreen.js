74f49499c4434d5815831e3dd002b23b69948cdd
PointCodeZero <lucas_ftc@hotmail.com>
2019-03-01 09:20:44 -0500

    initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    // header: null
+    title: 'Home'
+  };
+
+  render() {
+    return <View style={styles.container} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
