47b6573e2f98383d6cdd1cfb719d365177d659e3
PointCodeZero <lucas_ftc@hotmail.com>
2019-03-02 15:10:31 -0500

    removed console logs

--- screens/AlbumDetailScreen.js
+++ screens/AlbumDetailScreen.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
-import { Text, Icon } from 'react-native-elements';
-import { CardList } from '../components/CardList';
-import { SearchText } from '../components/SearchText';
-import * as actions from '../actions';
+import { ScrollView, StyleSheet, View, Text } from 'react-native';
 
 export default class AlbumDetailScreen extends React.Component {
   static navigationOptions = {
     title: 'Album Detail'
   };
 
+  componentDidMount() {
+    const album = this.props.navigation.getParam('album', {});
+  }
+
   render() {
     const album = this.props.navigation.getParam('album', {});
-    return <Text>Album title is {album.title}</Text>;
+    return (
+      <ScrollView style={styles.container}>
+        <View>
+          <Text>Album title is {album.title}</Text>
+        </View>
+      </ScrollView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff'
+  }
+});
