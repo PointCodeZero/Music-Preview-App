7310ac4dd835ee6e4692c587bcfc27b625400cd3
PointCodeZero <lucas_ftc@hotmail.com>
2019-03-18 17:48:22 -0400

    added camera screen for learning purposes only

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,98 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { Camera, Permissions, FileSystem } from 'expo';
 
-export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
+export default class CameraExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back
   };
 
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  async snapPhoto() {
+    console.log('Button Pressed');
+    if (this.camera) {
+      console.log('Taking photo');
+      const options = {
+        quality: 1,
+        base64: true,
+        fixOrientation: true,
+        exif: true
+      };
+      await this.camera.takePictureAsync(options).then(async photo => {
+        photo.exif.Orientation = 1;
+        // method: POST
+        // content-type: "application/octet-stream"
+        // body: photo.base64
+      });
+    }
+  }
+
   render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
-    );
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera
+            style={{ flex: 1 }}
+            type={this.state.type}
+            ref={ref => {
+              this.camera = ref;
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row'
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center'
+                }}
+                onPress={() => {
+                  this.setState({
+                    type:
+                      this.state.type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back
+                  });
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}
+                >
+                  {' '}
+                  Flip{' '}
+                </Text>
+
+                <View>
+                  <Icon
+                    onPress={this.snapPhoto.bind(this)}
+                    raised
+                    name="circle"
+                    type="font-awesome"
+                    color="#f50"
+                    size={30}
+                  />
+                </View>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
